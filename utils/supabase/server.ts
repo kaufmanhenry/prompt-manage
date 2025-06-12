175e87532559b477592822bc7d4a91ae62009a34
Henry Kaufman <hkaufman19@gmail.com>
2025-06-12 17:11:22 -0400

    fix

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,8 +1,8 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export const createClient = () => {
-  const cookieStore = cookies()
+export const createClient = async () => {
+  const cookieStore = await cookies()
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -16,17 +16,17 @@ export const createClient = () => {
           try {
             cookieStore.set({ name, value, ...options })
           } catch (error) {
-            // Handle cookie errors
+            console.error(error)
           }
         },
         remove(name: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value: '', ...options })
           } catch (error) {
-            // Handle cookie errors
+            console.error(error)
           }
         },
       },
     }
   )
-} 
+}

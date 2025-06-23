bd1fc9674df9e489d8b97d9c84d3166f4775f168
Mike <mike@filtergrade.com>
2025-06-23 16:44:52 -0400

    GA Integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Layout } from "@/components/Layout";
 import { PromptProvider } from "@/components/PromptContext";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeHtmlScript } from "@/components/ThemeHtmlScript";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-J61N380PQS"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-J61N380PQS');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <ThemeHtmlScript />
         <Providers>

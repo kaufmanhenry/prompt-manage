37ddc03c9ea4db96136774a91f025430e2f1f2c9
Henry Kaufman <hkaufman19@gmail.com>
2025-06-30 16:36:10 -0400

    clean footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,46 +1,45 @@
 'use client'
 
+import { Sparkles } from 'lucide-react'
 import Link from 'next/link'
 
 export function Footer() {
   return (
-    <footer className="border-t py-8 bg-white dark:bg-gray-950">
+    <footer className="border-t py-3 bg-white dark:bg-gray-950">
       <div className="mx-auto px-4 flex flex-col md:flex-row justify-between items-start md:items-center gap-8 md:gap-0">
-        <div className="flex flex-col md:flex-row gap-12 w-full">
-          <div>
-            <h3 className="text-sm font-semibold mb-3 text-gray-700 dark:text-gray-200 tracking-tight">
-              Prompt Manage
-            </h3>
-            <ul className="text-sm flex items-center gap-2">
-              <li>
-                <Link
-                  href="/about"
-                  className="hover:underline text-gray-600 dark:text-gray-300"
-                >
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/terms"
-                  className="hover:underline text-gray-600 dark:text-gray-300"
-                >
-                  Terms
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/privacy"
-                  className="hover:underline text-gray-600 dark:text-gray-300"
-                >
-                  Privacy
-                </Link>
-              </li>
-            </ul>
+        <div className="flex gap-2 w-full items-center">
+          <div className="flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg h-6 w-6 mr-2">
+            <Sparkles className="w-4 h-4 text-white" />
           </div>
+          <ul className="text-sm flex items-center gap-2">
+            <li>
+              <Link
+                href="/about"
+                className="hover:underline text-gray-600 dark:text-gray-300"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/terms"
+                className="hover:underline text-gray-600 dark:text-gray-300"
+              >
+                Terms
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/privacy"
+                className="hover:underline text-gray-600 dark:text-gray-300"
+              >
+                Privacy
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className="mt-8 md:mt-0 text-xs text-gray-500 dark:text-gray-400 w-full text-center md:text-right">
-          Copyright © 2025 Prompt Manage LLC. All rights reserved.
+          © 2025 Prompt Manage LLC.
         </div>
       </div>
     </footer>

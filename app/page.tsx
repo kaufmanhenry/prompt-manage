2803d043b7626f49c8b02aca6246c651c2026cb4
Mike Moloney <mike@filtergrade.com>
2025-11-20 09:01:38 -0500

    feat: add root page to redirect to default locale /en

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation'
+
+// This page only handles the root route (/)
+// It redirects to the default locale (/en)
+export default function RootPage() {
+    redirect('/en')
+}

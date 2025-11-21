c3d0695f3c9a202b6db995f94a3d5715bb5de28d
Mike <mike@filtergrade.com>
2025-11-21 09:59:37 -0500

    Fix formatting issues

--- app/[locale]/directory/submit/page.tsx
+++ app/[locale]/directory/submit/page.tsx
@@ -135,15 +135,15 @@ export default function SubmitToolPage() {
           .filter((u) => u),
         integrations: data.integrations
           ? data.integrations
-            .split(',')
-            .map((i) => i.trim())
-            .filter((i) => i)
+              .split(',')
+              .map((i) => i.trim())
+              .filter((i) => i)
           : [],
         ai_models_used: data.ai_models_used
           ? data.ai_models_used
-            .split(',')
-            .map((m) => m.trim())
-            .filter((m) => m)
+              .split(',')
+              .map((m) => m.trim())
+              .filter((m) => m)
           : [],
       }
 
